10fe719fa454447ed2a502478ac9e1a543b532c1
rociopmz <43999716+rociopmz@users.noreply.github.com>
2021-09-15 01:05:08 -0500

    modificaciones en src

--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.css'
 
+const WithRouter = () => (
+
+  <BrowserRouter>
+    <App/>
+  </BrowserRouter>
+)
+//substitute original content in ReactDOM.render
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <WithRouter />,
   document.getElementById('root')
 );
 
